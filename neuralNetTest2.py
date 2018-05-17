bda73e85b04deda02df464d2c70a5fb5ccf4a967
RylanSteinkey <35366065+RylanSteinkey@users.noreply.github.com>
2018-05-17 10:32:29 -0600

    Add files via upload

--- neuralNetTest2.py
+++ neuralNetTest2.py
@@ -20,9 +20,12 @@ def make_name_3D(data):
 
 train_names = np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1])
 
-train_list       = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0]
-bovine_test_list = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-human_test_list  = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1]
+
+train_list       = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0, 1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0]
+bovine_test_list = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+human_test_list  = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1]
+
+
 train_list1 = [bool(x) for x in train_list]
 bovine_test_list1 = [bool(x) for x in bovine_test_list]
 human_test_list1 = [bool(x) for x in human_test_list]
@@ -34,7 +37,7 @@ human_mask = np.array(human_test_list1)
 train_data = kmermatrix[train_mask, :]
 bovine_test = kmermatrix[bovine_mask, :]
 human_test = kmermatrix[human_mask, :]
-
+#print(train_data.shape)
 if len(train_data.shape) == 2:
         train_data = make3D(train_data)
         bovine_test = make3D(bovine_test)
@@ -44,23 +47,25 @@ if len(train_data.shape) == 2:
 
 train_names = to_categorical(train_names, 2)
 
-print(train_data.shape)
-print(train_names.shape)
+#print(train_data.shape)
+#print(train_names.shape)
+#print(train_names)
 
 """
 model = Sequential()
-#model.add(Conv1D(filters=10, kernel_size=1, activation='relu', input_shape=(1,train_data.shape[2])))
+model.add(Conv1D(filters=10, kernel_size=1, activation='relu', input_shape=(1,train_data.shape[2])))
 model.add(Flatten())
 model.add(Dense(2, activation='softmax'))
 model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
 model.fit(train_data, train_names, epochs =25, verbose=1)
 """
-
 model = Sequential()
 model.add(Conv1D(filters=10, kernel_size=1, activation='relu', input_shape=(1,train_data.shape[2])))
 model.add(Flatten())
-model.add(Dense(12,input_dim=3634442,kernel_initializer='uniform',activation='relu'))
-model.add(Dense(8,kernel_initializer='uniform',activation='relu'))
+model.add(Dense(70,input_dim=3634442,kernel_initializer='uniform',activation='relu'))
+model.add(Dense(50,kernel_initializer='uniform',activation='relu'))
+model.add(Dense(30,kernel_initializer='uniform',activation='relu'))
+model.add(Dense(12,kernel_initializer='uniform',activation='relu'))
 model.add(Dense(2,kernel_initializer='uniform',activation='sigmoid'))
 model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
 model.fit(train_data, train_names, epochs =50, verbose=1)
