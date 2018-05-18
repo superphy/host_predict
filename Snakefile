e4baaa0db61823c458d7d543b14c08b1f3a0a54c
JanMoat <jmoat@sfu.ca>
2018-05-18 10:45:56 -0600

    Add files via upload

--- Snakefile
+++ Snakefile
@@ -1,60 +1,32 @@
-configfile: "config.yaml"
-
-ruleorder: all > kmer_count > fa_dump > make_master > update_master > make_zarr
+ids, = glob_wildcards("genomes/{id}.fasta")
 
 rule all:
   input:
-    expand("results/human/.{human_samples}", human_samples=config["human_samples"]),
-    expand("results/bovine/.{bovine_samples}", bovine_samples=config["bovine_samples"]),
-    expand("touch/human/.{human_samples}", human_samples=config["human_samples"]),
-    expand("touch/bovine/.{bovine_samples}", bovine_samples=config["bovine_samples"])
-
+    "touchfile.txt"
 
 rule kmer_count:
-   input:
-       "data/{species}/{sample}.fasta"
-   output:
-       temp("results/{species}/{sample}.jf")
-   shell:
-       "jellyfish count -m 11 -s 100M {input} -o {output}"
-
+  input:
+    "genomes/{id}.fasta"
+  output:
+    temp("results/{id}.jf")
+  shell:
+    "jellyfish count -m 11 -s 100M {input} -o {output}"
 
 rule fa_dump:
   input:
-    "results/{species}/{sample}.jf"
+    "results/{id}.jf"
   output:
-    "results/{species}/{sample}.fa"
+    "results/{id}.fa"
   shell:
     "jellyfish dump {input} > {output}"
 
 
 rule make_master: 
   input:
-    "results/{species}/{sample}.fa"
+    expand("results/{id}.fa", id=ids)
   output:
-    touch("touch/{species}/.{sample}")
+    touch("touchfile.txt")
   shell:
-    "python createmaster.py {input}"
+   "python createmaster.py {input}"
 
 
-rule update_master: 
-  input:
-    "touch/human/.h-ECI-0008"
-    #"touch/{species}/.{sample}"
-  output:
-    #"touch/touch/{species}/.{sample}"
-    touch("touch/touchfile")
-  shell:
-    "python updatemaster.py"
-
-
-rule make_zarr: 
-  input:
-    a = "results/{species}/{sample}.fa",
-    #b = "touch/touch/{species}/.{sample}",
-    c = "touch/touchfile"
-  output:
-    touch("results/{species}/.{sample}")
-  threads: 1000000000
-  shell:
-    "python directload.py {input.a}"
--- createmaster.py
+++ createmaster.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 from Bio import SeqIO
+from pathlib import *
+import os
 import sys
 import lmdb
-import zarr
+
 
 def print_env(env):
 	"""
@@ -25,34 +27,57 @@ if __name__ == "__main__":
 	Col stores kmer sequence and column number.
 	Row stores genome ID and row number.
 	"""
-
 	# Create environments
 	colenv = lmdb.Environment(b'coldb', max_dbs=100, map_size=5000000000)
 	rowenv = lmdb.Environment(b'rowdb', max_dbs=100, map_size=5000000000)
 
-	filename = sys.argv[1]            # The filepath is given in the first argument
-	genomeid = filename.split('/')[2] # Get filename.fa
-	genomeid = genomeid.split('.')[0] # Get filename
+	p = Path('./results')
+	for filename in p.iterdir():
+		# Get the genomeid from the filepath
+		genomeid = os.path.basename(filename)
 
 
-	# Fill in the row environment.
-	with rowenv.begin(write=True) as txn:
-		# Add the genome ID as the key. Initially the value is 0.
-		# Files aren't given to this script in alphabetical order,
-		# so the value (row#) will be input later (updatemaster.py)
-		txn.put(genomeid.encode('ascii'), '0'.encode('ascii'), overwrite=True)
+		rowindex = 0
+		# Fill in the row environment.
+		with rowenv.begin(write=True) as txn:
+			# key = GenomeID & value = row index
+			# Note that files passed in alphabetical order
+			strindex = str(rowindex)
+			txn.put(genomeid.encode('ascii'), strindex.encode('ascii'), overwrite=True)
+			rowindex+=1
 
-	# Fill in the colenv
+		# Fill in the colenv
+		with colenv.begin(write=True) as txn:
+			for record in SeqIO.parse(filename, "fasta"):
+				# Add the sequence as the key. Initially the value is 0.
+				# Files aren't given to this script in alphabetical order,
+				# so the value (col#) will be input later (updatemaster.py)
+				kmerseq = record.seq
+				kmerseq = kmerseq._get_seq_str_and_check_alphabet(kmerseq)
+				txn.put(kmerseq.encode('ascii'), '0'.encode('ascii'), overwrite=True)
+
+	# After all sequences have been inserted in alphabetical order, assign their index
 	with colenv.begin(write=True) as txn:
-		for record in SeqIO.parse(filename, "fasta"):
-			# Add the sequence as the key. Initially the value is 0.
-			# Files aren't given to this script in alphabetical order,
-			# so the value (col#) will be input later (updatemaster.py)
-			kmerseq = record.seq
-			kmerseq = kmerseq._get_seq_str_and_check_alphabet(kmerseq)
-			txn.put(kmerseq.encode('ascii'), '0'.encode('ascii'), overwrite=True)
+		cursor = txn.cursor()
+		for (index,(seq, value)) in enumerate(cursor):
+			seq = seq.decode('utf-8')
+			index = str(index)
+			txn.put(seq.encode('ascii'), index.encode('ascii'), overwrite=True)
+
+'''
+	# Initialize the kmermatrix
+	numrows = rowenv.stat()['entries']
+	numrows = int(numrows)
+	numcols = colenv.stat()['entries']
+	numcols = int(numcols)
+
+	kmermatrix = np.zeros((numrows,numcols))
+	np.save('kmermatrix.npy', kmermatrix)
+'''
 
 	#print_env(rowenv)
 	#print_env(colenv)
+
+
 
 
